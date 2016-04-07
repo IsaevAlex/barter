66b4cc69082294ebf1551678e71872939df74d92
Cronos <lexsandr46@gmail.com>
2016-04-07 20:01:59 +0300

    Edit order_mailer.rb

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,10 +2,8 @@ class OrderMailer < ApplicationMailer
 	def order_create(order)
 		@order = order
 		
-		Mail.deliver do
-  			to: @order.recipient_user.email
-  			subject: "Вам прислали сообщение",
-			from: "services@mydomain.com"
-		end
+		mail(to: @order.recipient_user.email,
+			 subject: "Вам прислали сообщение",
+			 from: "services@mydomain.com") 
 	end
 end
