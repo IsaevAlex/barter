f025c1766c4b11e55927a3567fc4323092196390
Cronos <lexsandr46@gmail.com>
2016-04-08 21:43:22 +0300

    Edit production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,13 +85,13 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false  
   config.action_mailer.default :charset => "utf-8"  
   config.action_mailer.smtp_settings = {  
-    address: "smtp.gmail.com",
+    address: "smtp.sendgrid.net",
     port: 587,
     domain: "barterco.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    :user_name => 'lexsandr46@gmail.com',
-    :password => 'asensible1995cronos2025'
+    :user_name => 'app49224341@heroku.com',
+    :password => 'pv7ztdpi9206'
   }
 
 
